2448a44aaba387fc3f4cf63c4b737e7e2a0b99e5
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-05-12 20:57:01 +0200

    Added WiFi connecting procedure

--- include/Debug.h
+++ include/Debug.h
@@ -8,3 +8,17 @@
  * DEBUG_MODE definition line:
  * //#define DEBUG_MODE
  *******************************************************************/
+
+#ifndef DEBUG_H
+    #define DEBUG_H
+    #ifdef DEBUG_MODE // debug development settings
+        // calls to DEBUG will be replaced with Serial.print command
+        #define DEBUG(x) Serial.print(x)
+        // calls to DEBUGLN will be replaced with Serial.println command
+        #define DEBUGLN(x) Serial.println(x)
+    #else // production settings
+        // calls to DEBUG and DEBUGLN will be replaced with nothing
+        #define DEBUG(x)
+        #define DEBUGLN(x)
+    #endif
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #define DEBUG_MODE // please read the instructions in include/Debug.h file
 
 #include <Arduino.h>
+#include <WiFi.h>
 #include "Credentials.h" // please read the instructions in include/CredentialsTemplate.h file
 #include "Debug.h"
+
+void connectToWiFi() {
+  // Connect to WiFi network
+  DEBUG("Connecting to WiFi network: ");DEBUGLN(WIFI_SSID);
+  WiFi.mode(WIFI_STA);
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  }
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    DEBUGLN("WiFi connected!");
+  } 
+    else {
+    DEBUGLN("WiFi not connected, check your credentials");
+    // TODO production code should go to deep sleep immediately
+  }
+}
 
 void setup() {
   // if in debug mode, start the serial connection
@@ -12,6 +29,7 @@ void setup() {
     delay(5000); // give us time to open serial monitor if closed
   #endif
   DEBUGLN("Debug mode on"); // let us know you're ready
+  connectToWiFi();
 }
 
 void loop() {
