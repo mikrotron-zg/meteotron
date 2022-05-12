8c6ab2f1df4182e7e40d166fae25bca35b2e7e92
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-05-12 19:50:19 +0200

    Added debugging over serial connection to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+#define DEBUG_MODE // please read the instructions in include/Debug.h file
+
 #include <Arduino.h>
-// please read the instructions in include/CredentialsTemplate.h first
-#include "Credentials.h"
+#include "Credentials.h" // please read the instructions in include/CredentialsTemplate.h file
+#include "Debug.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  // if in debug mode, start the serial connection
+  #ifdef DEBUG_MODE
+    Serial.begin(115200); // preffered baud rate for ESP32's
+    while(!Serial); // wait for serial connection to be ready
+    delay(5000); // give us time to open serial monitor if closed
+  #endif
+  DEBUGLN("Debug mode on"); // let us know you're ready
 }
 
 void loop() {
