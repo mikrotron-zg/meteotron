0b2c90b87b6840f9d506ad161be104d0de885518
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-05-23 14:31:13 +0200

    Added Configuration.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #define DEBUG_MODE // please read the instructions in include/Debug.h file
-//#define PIN_I2C_POWER 7
 
 #include <Arduino.h>
 #include <WiFi.h>
 #include <SPI.h>
 #include <Adafruit_LC709203F.h>
 #include "BME680-SOLDERED.h"
+#include "Configuration.h" // please read the instructions in include/Configuration.h file
 #include "Credentials.h" // please read the instructions in include/CredentialsTemplate.h file
 #include "Debug.h"
-
-Adafruit_LC709203F lc;
-BME680 bme680;
 
 bool connectToWiFi() {
   // Connect to WiFi network
@@ -38,24 +35,31 @@ void powerOn() {
 
 void checkBattery() {
   // Get battery readings
+  Adafruit_LC709203F lc;
   if (!lc.begin()) {
     DEBUGLN("Could not find Adafruit LC709203F or battery not pluged in!");
     return;
   }
-
-  DEBUG("Batt Voltage: "); DEBUGLN(lc.cellVoltage());
-  DEBUG("Batt Percent: "); DEBUGLN(lc.cellPercent());
+  sensorData.batteryVoltage = lc.cellVoltage();
+  delay(25);
+  sensorData.batteryPercentage = lc.cellPercent();
+  DEBUG("Batt Voltage: "); DEBUGLN(sensorData.batteryVoltage);
+  DEBUG("Batt Percent: "); DEBUGLN(sensorData.batteryPercentage);
 }
 
 void readBME() {
+  // Read and save BME680 data
+  BME680 bme680;
   bme680.begin();
   delay(500);
-  DEBUG("Temperature: "); DEBUGLN(bme680.readTemperature());
-  delay(250);
-  DEBUG("Humidity: "); DEBUGLN(bme680.readHumidity());
-  delay(250);
-  DEBUG("Atm. pressure: "); DEBUGLN(bme680.readPressure());
-  // TODO use the altitude correction for pressure reading
+  sensorData.temperature = bme680.readTemperature();
+  delay(25);
+  sensorData.humidity = bme680.readHumidity();
+  delay(25);
+  sensorData.pressure = bme680.readPressure() + PA_PER_METER*ALTITUDE;
+  DEBUG("Temperature: "); DEBUGLN(sensorData.temperature);
+  DEBUG("Humidity: "); DEBUGLN(sensorData.humidity);
+  DEBUG("Atm. pressure: "); DEBUGLN(sensorData.pressure);
 }
 
 void gotoSleep() {
@@ -73,10 +77,12 @@ void setup() {
   if (!connectToWiFi()) gotoSleep();
   powerOn(); // power on the I2C bus
   delay(250);
-  //checkBattery();
+  checkBattery();
   readBME();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(10000);
+  readBME();
 }
