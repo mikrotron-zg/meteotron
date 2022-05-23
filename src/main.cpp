83c8a333b319d62b26e716a8816a06c2830ed1a5
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-05-23 13:55:09 +0200

    Added BME280/680 library, tested atmospheric data

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 #include <WiFi.h>
 #include <SPI.h>
 #include <Adafruit_LC709203F.h>
+#include "BME680-SOLDERED.h"
 #include "Credentials.h" // please read the instructions in include/CredentialsTemplate.h file
 #include "Debug.h"
 
 Adafruit_LC709203F lc;
+BME680 bme680;
 
 bool connectToWiFi() {
   // Connect to WiFi network
@@ -25,16 +27,17 @@ bool connectToWiFi() {
   return false;
 }
 
-void checkBattery() {
-  // Get battery readings
- 
-  // turn on the I2C power by setting pin to opposite of 'rest state'
+void powerOn() {
+// turn on the I2C power by setting pin to opposite of 'rest state'
   pinMode(PIN_I2C_POWER, INPUT);
   delay(1);
   bool polarity = digitalRead(PIN_I2C_POWER);
   pinMode(PIN_I2C_POWER, OUTPUT);
   digitalWrite(PIN_I2C_POWER, !polarity);
+}
 
+void checkBattery() {
+  // Get battery readings
   if (!lc.begin()) {
     DEBUGLN("Could not find Adafruit LC709203F or battery not pluged in!");
     return;
@@ -42,6 +45,17 @@ void checkBattery() {
 
   DEBUG("Batt Voltage: "); DEBUGLN(lc.cellVoltage());
   DEBUG("Batt Percent: "); DEBUGLN(lc.cellPercent());
+}
+
+void readBME() {
+  bme680.begin();
+  delay(500);
+  DEBUG("Temperature: "); DEBUGLN(bme680.readTemperature());
+  delay(250);
+  DEBUG("Humidity: "); DEBUGLN(bme680.readHumidity());
+  delay(250);
+  DEBUG("Atm. pressure: "); DEBUGLN(bme680.readPressure());
+  // TODO use the altitude correction for pressure reading
 }
 
 void gotoSleep() {
@@ -57,7 +71,10 @@ void setup() {
   #endif
   DEBUGLN("Debug mode on"); // let us know you're ready
   if (!connectToWiFi()) gotoSleep();
-  checkBattery();
+  powerOn(); // power on the I2C bus
+  delay(250);
+  //checkBattery();
+  readBME();
 }
 
 void loop() {
